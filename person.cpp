ad122265134f431ba40b6de0e541f28e93ba9736
MarioVelasquezN <41805686+MarioVelasquezN@users.noreply.github.com>
2020-02-10 15:56:24 -0600

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,26 @@
+#include "person.h";
+#include <iostream>
+using namespace std;
+
+void Person::Print(){
+    cout << "Last Name '" << LastName << "'\n"
+    << "First Name '" << FirstName << "'\n"
+    << "Address '" << Address << "'\n"
+    << "City '" << City << "'\n"
+    << "State '" << State << "'\n"
+    << "Zip Code '" << ZipCode << "'\n"
+    << flush;
+}
+
+Person::Person(){
+    LastName[0]=0;
+    FirstName[0]=0;
+    Address[0]=0;
+    City[0]=0;
+    State[0]=0;
+    ZipCode[0]=0;
+}
+
+//hacer un struct, usar la clase de persona//
+//struct encapsular, cuando le demos print imprimamos el struct.
+//
